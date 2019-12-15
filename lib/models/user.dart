a72ce201290e091686de038b3d5c92123575ba32
SirAnxious <siranxiousalot@mail.com>
2019-12-15 23:39:25 +0200

    FORGOT
    THIS

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,14 @@
 class User {
   final String uid;
-
   User({ this.uid });
 
 }
+
 class Data{
   final String uid;
   final String name;
   final String majors;
+  final String enrolledCourses;
 
-  Data({this.uid,this.name,this.majors});
+  Data({this.uid,this.name,this.majors,this.enrolledCourses});
 }
